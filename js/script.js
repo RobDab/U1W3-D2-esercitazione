b3ba59e61fd6e2cbcb932472945d9bd4a6432a27
RobDab <roberto.dabb@gmail.com>
2022-09-13 17:42:57 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,70 @@
+let name = prompt(`inserisci qui il tuo nome`,`Mario Rossi`)
+console.log('ciao ' + name)
+
+console.log('calcolatore di valore assoluto') 
+let number = Number(prompt('Ciao ' + name + '. Inserisci il numero di cui calcolare il |VA|...'))
+
+console.log('|' + number + '| = ' + valoreAssoluto(number))
+
+document.getElementById('risultato').innerHTML = valoreAssoluto(number)
+
+
+console.log('eseguito')
+
+
+const piGreco = 3.14159
+console.log('la circonferenza di un cerchio di D=1 è ' + piGreco)
+
+
+
+
+
+let heisenberg = (1 == 0)
+console.log(heisenberg)
+console.log("L'assegnazione della variabile ha restituito " + heisenberg)
+
+
+
+
+// nome troopo lungo?
+const rispostaAllaDomandaFondamentaleSullaVitaLUuniversoEeTuttoQuanto = 42
+
+console.log ('Risposta...etc è: ' + rispostaAllaDomandaFondamentaleSullaVitaLUuniversoEeTuttoQuanto)
+
+let doppio = rispostaAllaDomandaFondamentaleSullaVitaLUuniversoEeTuttoQuanto * 2
+
+console.log('variabile doppio è: ' + doppio)
+
+
+// RispostaAllaDomandaFondamentaleSullaVitaLUuniversoEeTuttoQuanto = 'ciao'
+rispostaAllaDomandaFondamentaleSullaVitaLUuniversoEeTuttoQuanto += 42
+console.log('ora Risposta..etc è: ' + rispostaAllaDomandaFondamentaleSullaVitaLUuniversoEeTuttoQuanto)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// FUNZIONI
+function valoreAssoluto(x){
+    if(x>0 || x===0){
+        return(x)
+    }
+    else{
+        return(-x)
+    }
+}
